72d0fe0b32dff5a38f14c46baed2e7feecc52005
house <house@housedemac-mini.local>
2017-01-18 11:17:36 +0800

    Initial Commit

---
+++ IntergrationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntergrationDemo
+//
+//  Created by house on 2017/1/18.
+//  Copyright © 2017年 house. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntergrationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntergrationDemo
+//
+//  Created by house on 2017/1/18.
+//  Copyright © 2017年 house. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
